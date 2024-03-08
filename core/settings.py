d1b159ff6010d6bb6a6e4bdc71640de14b286e8e
Mohan Pandit <mohanpandit131@gmail.com>
2024-03-08 13:25:38 +0530

    test

--- core/settings.py
+++ core/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_ip_geolocation.middleware.IpGeolocationMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from django_ip_geolocation.decorators import with_ip_geolocation
 
 
 
-
-
+@with_ip_geolocation
 def get_user_location(request):
+    try:
+        print(request.geolocation)
+    except Exception as e:
+        print(e)
     print(request.META['HTTP_USER_AGENT'])
     remote_addr = request.META.get('HTTP_X_FORWARDED_FOR')
     if remote_addr:
